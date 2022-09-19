243f66f90b6421f15af01fff61d3f358baf59bc2
rg3915 <regis42santos@gmail.com>
2022-09-18 23:01:53 -0300

    Criando fixtures

--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -1,43 +1,27 @@
-from splinter import Browser
 from ward import test
 
-from app import create_app, db
+from __tests__.fixtures import browser
+from app import db
 from app.models import Post
 
 
-# @test('Página principal deve estar online.')
-# def _():
-#     app = create_app()
-#     app.testing = True
-#     browser = Browser('flask', app=app)
+@test('Página principal deve estar online.')
+def _(browser=browser):
+    browser.visit('/')
 
-#     browser.visit('/')
-#     assert browser.is_text_present('Olá, Flask!')
+    assert browser.is_text_present('Olá, Flask!')
 
 
 @test('Visitante acessa a página principal e vê posts.')
-def _():
-    app = create_app()
-    app.testing = True
+def _(browser=browser):
+    post = Post(
+        title='Curtindo as férias em Salvador',
+        published=True,
+        text='text...'
+    )
+    db.session.add(post)
+    db.session.commit()
 
-    context = app.test_request_context()
-    context.push()
+    browser.visit('/')
 
-    with app.test_client():
-        db.create_all()
-
-        post = Post(
-            title='Curtindo as férias em Salvador',
-            published=True,
-            text='text...'
-        )
-        db.session.add(post)
-        db.session.commit()
-
-        browser = Browser('flask', app=app)
-        browser.visit('/')
-
-        assert browser.is_text_present('Curtindo as férias em Salvador')
-
-        db.drop_all()
-        db.session.remove()
+    assert browser.is_text_present('Curtindo as férias em Salvador')
