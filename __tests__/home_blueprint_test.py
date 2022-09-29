a846059e0b6a73489d2ba491d5defbbe47a050db
rg3915 <regis42santos@gmail.com>
2022-09-29 04:59:56 -0300

    Construindo dados dinamicamente com factory_boy

--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -1,10 +1,12 @@
-from faker import Faker
+# from faker import Faker
 from flask import url_for
 from ward import test
 
+from __tests__.factories.post_factory import PostFactory
 from __tests__.fixtures import browser
-from app import db
-from app.models import Post
+
+# from app import db
+# from app.models import Post
 
 
 @test('Página principal deve estar online.')
@@ -16,14 +18,12 @@ def _(browser=browser):
 
 @test('Visitante acessa a página principal e vê posts.')
 def _(browser=browser):
-    faker = Faker()
-    post = Post(
-        title=faker.paragraph(),
-        published=True,
-        text='text...'
-    )
-    db.session.add(post)
-    db.session.commit()
+    # post = Post(
+    #     title=faker.paragraph(),
+    #     published=True,
+    #     text='text...'
+    # )
+    post = PostFactory.create()
 
     browser.visit(url_for('home.index'))
 
