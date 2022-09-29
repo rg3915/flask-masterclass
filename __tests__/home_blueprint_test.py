82058002a713aaa265b08cf5c18dd145af77f2a4
rg3915 <regis42santos@gmail.com>
2022-09-29 04:48:08 -0300

    Trabalhando com dados fake.

--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -1,3 +1,5 @@
+from faker import Faker
+from flask import url_for
 from ward import test
 
 from __tests__.fixtures import browser
@@ -7,21 +9,29 @@ from app.models import Post
 
 @test('Página principal deve estar online.')
 def _(browser=browser):
-    browser.visit('/')
+    browser.visit(url_for('home.index'))
 
     assert browser.is_text_present('Olá, Flask!')
 
 
 @test('Visitante acessa a página principal e vê posts.')
 def _(browser=browser):
+    faker = Faker()
     post = Post(
-        title='Curtindo as férias em Salvador',
+        title=faker.paragraph(),
         published=True,
         text='text...'
     )
     db.session.add(post)
     db.session.commit()
 
-    browser.visit('/')
+    browser.visit(url_for('home.index'))
 
-    assert browser.is_text_present('Curtindo as férias em Salvador')
+    assert browser.is_text_present(post.title)
+
+
+@test('Visitante não consegue visualizar posts.')
+def _(browser=browser):
+    browser.visit(url_for('home.index'))
+
+    assert browser.is_text_present('Nenhum post cadastrado!')
